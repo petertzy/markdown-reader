8118162693ae051d3df5fca4acf838dae292a5fe
Matthew Emerson <matthew.j.emerson@durham.ac.uk>
2026-02-25 15:29:41 +0000

    Added documentation for utils.py

--- markdown_reader/utils.py
+++ markdown_reader/utils.py
@@ -3,7 +3,15 @@ import sys
 import tempfile
 
 
-def get_resource_path(filename):
+def get_resource_path(filename): # Potentially unused function?
+    """
+    Gets the filepath of a resource depending on the system's attributes.
+
+    :param string filename: The file name for the resource to be returned.
+
+    :return: A string with the filename joined onto its relevant file path.
+    """
+    
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, filename)
     elif getattr(sys, 'frozen', False):
@@ -13,6 +21,12 @@ def get_resource_path(filename):
 
 
 def get_preview_file():
+    """
+    Returns the file path for the Markdown preview file based on the file's directory.
+
+    :return: A string containing the file path for the temporary HTML preview file.
+    """
+    
     preview_path = os.path.join(tempfile.gettempdir(), "markdown_preview.html")
     with open(preview_path, 'w', encoding='utf-8') as f:
         f.write("")
