7e2056795a596069bf4242fc6fe4ea99f7499af6
Matthew Emerson <matthew.j.emerson@durham.ac.uk>
2026-02-25 15:24:38 +0000

    Removed double import of os from utils.py

--- markdown_reader/utils.py
+++ markdown_reader/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import tempfile
 
+
 def get_resource_path(filename):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, filename)
@@ -10,11 +11,9 @@ def get_resource_path(filename):
     else:
         return os.path.join(os.path.abspath("."), filename)
 
-import os
 
 def get_preview_file():
     preview_path = os.path.join(tempfile.gettempdir(), "markdown_preview.html")
     with open(preview_path, 'w', encoding='utf-8') as f:
         f.write("")
     return preview_path
-
